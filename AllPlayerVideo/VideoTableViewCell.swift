3aa5510cfced867105aad69f465b8091d3134bd0
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-26 16:04:10 -0700

    Commit

---
+++ AllPlayerVideo/VideoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  VideoTableViewCell.swift
+//  AllPlayerVideo
+//
+//  Created by Arpit iOS Dev. on 26/06/24.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
